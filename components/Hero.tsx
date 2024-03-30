b7505568aab61436981d406df57f18164a57e1d3
aayushker <singhaayushker@gmail.com>
2024-03-30 18:16:41 +0530

    Fixed the svg bug

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,9 +26,10 @@ const Hero = () => {
 
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero image" fill className="object-contain" />
-          <div className="hero__image-overlay"></div>
+          <Image src="/hero.png" alt="hero image" fill className="object-contain" />     
         </div>
+          <div className="hero__image-overlay"></div>
+   
       </div>
     </div>
   );

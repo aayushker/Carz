4c6ac048265089e4eacf717683a1cb527299a96f
aayushker <singhaayushker@gmail.com>
2024-03-31 20:54:50 +0530

    Staered making main display section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,18 @@ export default function Home() {
   return (
     <main className="overflow-hidden ">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">
+            Car Catalogue
+          </h1>
+          <p>Explore the world of cars</p>
+        </div>
+
+        <div className="home__filters">
+          <
+        </div>
+      </div>
     </main>
   );
 }

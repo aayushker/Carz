8c62d477a0d51dcc6630a54e34b8eda2f865b3f6
aayushker <singhaayushker@gmail.com>
2024-04-18 20:30:06 +0530

    Updated the filters

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -7,16 +7,27 @@ const CustomButton = ({
   containerStyles,
   handleClick,
   btnType,
+  isDisabled,
 }: CustomButtonProps) => {
   return (
     <button
-      disabled={false}
-      type={btnType || "button"}
-      className={`custom-btn ${containerStyles}`}
-      onClick= {handleClick}
-    >
-      <span className={"flex-1"}>{title}</span>
-    </button>
+    disabled={isDisabled}
+    type={btnType || "button"}
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
+  >
+    <span className={`flex-1 ${textStyles}`}>{title}</span>
+    {rightIcon && (
+      <div className="relative w-6 h-6">
+        <Image
+          src={rightIcon}
+          alt="arrow_left"
+          fill
+          className="object-contain"
+        />
+      </div>
+    )}
+  </button>
   );
 };
 

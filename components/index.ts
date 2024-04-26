eff8993186661756c7bb288902079221880b586e
aayushker <singhaayushker@gmail.com>
2024-04-27 02:17:19 +0530

    Added some components

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,19 @@
-import Hero from "./Hero";
+import CarCard from "./CarCard";
 import CustomButton from "./CustomButton";
-import Navbar from "./Navbar";
+import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
+import NavBar from "./Navbar";
+import ShowMore from "./ShowMore";
+import SearchBar from "./Searchbar";
+import Hero from "./Hero";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+};

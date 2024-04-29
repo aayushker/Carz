dd900568f77f6446ec06bfb895b2986b1a8eea8a
aayushker <singhaayushker@gmail.com>
2024-04-29 20:20:55 +0530

    Fixed the class name error in production

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default async function Home({ searchParams }: HomeProps) {
           <section>
             <div className="home__cars-wrapper">
               {allCars?.map((car) => (
-                <CarCard car={car} className />
+                <CarCard car={car} />
               ))}
             </div>
 
